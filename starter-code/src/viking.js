73feefb262146326027b013c9868d55097c4ca33
Karla Lobo <kay@Karlas-MacBook-Pro.local>
2017-05-26 11:03:12 +0200

    updated 2.0

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,22 @@
 // Soldier
-function Soldier () {}
+function Soldier(health, strength) {
+    this.health = health;
+    this.strength = strength;
 
+    Soldier.prototype.attack = function() {
+    return this.strength;
+};
+
+    Soldier.prototype.receiveDamage = function(damage) {
+    this.damage = damage;
+    this.health = this.health - this.damage;
+    };
+}
 // Viking
-function Viking () {}
+    function Viking() {}
 
 // Saxon
-function Saxon () {}
+    function Saxon() {}
 
 // War
-function War () {}
+function War() {}
